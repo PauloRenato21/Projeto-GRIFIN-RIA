31bafa9ddf305c8c273583c7660388e9f931b2d0
Neves84 <gabrielzim_pn@hotmail.com>
2021-03-30 20:51:21 -0300

    ciramos um novo formulario pedido

--- php/enviapedido.php
+++ php/enviapedido.php
@@ -1,30 +1,24 @@
 <?php
     include_once "conexao.php";
 
-    $nome = $_POST['nomecompleto'];
-    $data = $_POST['data'];
-    $bolo = $_POST['nomebolo'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $telefone = $_POST['telefone'];
-    $cpf = $_POST['cpf'];
-    $pagamento = $_POST['pg'];
+    
+    
+    $valido = true;
+    $campos = Array("data","nomebolo","pg");
+    foreach ($campos as $campo){
+        if (empty($_POST[$campo])){
+            $valido = false;
+            header('location:../form_pedido.php?erro_pedido=1');
+        }
+    }       
 
-    echo $nome;
-    echo "<br>";
-    echo $data;
-    echo "<br>";
-    echo $bolo;
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo $endereco;
-    echo "<br>";
-    echo $telefone;
-    echo "<br>";
-    echo $cpf;
-    echo "<br>";
-    echo $pagamento;
-
+    if ($valido){
+        $data = $_POST['data'];
+        $bolo = $_POST['nomebolo'];
+        $pagamento = $_POST['pg']; 
+        $sql = 'insert into pedido(nome_bolo, forma_pagamento, data_pedido) values("'.$bolo.'", "'.$pagamento.'", "'.$data.'")';
+        $con -> exec($sql);
+        header('location:../form_pedido.php?erro_pedido=2')
+    }
 
 ?>
