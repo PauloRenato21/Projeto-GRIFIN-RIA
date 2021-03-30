399d13937b1939294179a822e316828f4bce4c70
PauloRenato21 <paulorenato401@gmail.com>
2021-03-30 09:40:06 -0300

    conexao.php BD

---
+++ php/conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'mysql:host=grifnoria.mysql.dbaas.com.br;dbname=grifnoria';
+    $user = 'grifnoria';
+    $password = 'qwe123qwe';
+
+    $con = new pdo($host, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+?>
