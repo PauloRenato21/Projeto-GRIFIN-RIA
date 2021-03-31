44718be1a4a190049a0544faf57203c080f169ba
PauloRenato21 <paulorenato401@gmail.com>
2021-03-31 16:07:57 -0300

    add page php

---
+++ php/conexao.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'mysql:host=grifnoria.mysql.dbaas.com.br;dbname=grifnoria';
+    $user = 'grifnoria';
+    $password = 'qwe123qwe';
+
+    ECHO phpinfo();
+
+    // $con = new pdo($host, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+    // echo $con;
+
+?>
