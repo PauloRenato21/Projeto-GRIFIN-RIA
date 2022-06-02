12a7f5c61c15036d2eead6d55c22c2c867c616ef
PauloRenato21 <paulorenato401@gmail.com>
2022-06-02 17:04:01 -0300

    modified conexao mysql database

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
-    $host = 'mysql:host=grifnoria.mysql.dbaas.com.br;dbname=grifnoria';
-    $user = 'grifnoria';
-    $password = 'qwe123qwe';
+    $host = 'mysql:host=sql205.epizy.com;dbname=epiz_31873051_topBolos';
+    $user = 'epiz_31873051';
+    $password = 'gdXpe2lmAhVrq7E';
 
     $con = new pdo($host, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
